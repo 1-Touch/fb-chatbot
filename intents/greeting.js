671f4afce528aaecc26e63f7cc2f64e03c0ef747
Ashish <cu.15bcs2313@gmail.com>
2020-03-10 18:46:32 +0530

    greeting text

---
+++ intents/greeting.js
@@ -0,0 +1,21 @@
+const sendText = require('../processes/sendText');
+
+const greetings = [
+  'Hello, sunshine!',
+  'Howdy, partner!',
+  'Hey, howdy, hi!',
+  'What’s kickin’, little chicken?',
+  'Peek-a-boo!',
+  'Howdy-doody!',
+  'Hey there, freshman!',
+  'Hi, mister!',
+  'I come in peace!',
+  'Put that cookie down!',
+  'Ahoy, matey!',
+  'Hiya!'
+];
+
+module.exports = function greeting(sender) {
+  const greetingText = Math.floor(Math.random() * greetings.length);
+  sendText(sender, greetingText);
+};
--- processes/processMessage.js
+++ processes/processMessage.js
@@ -1,24 +1,19 @@
+const greeting = require('../intents/greeting');
 const sendText = require('./sendText');
 
-module.exports = function processMessage(sender, text){
+module.exports = function processMessage(sender, text) {
   let message = text.toLowerCase();
-  console.log('sender details are ---', sender)
-  switch(message){
-    case 'hey' || 'hello' || 'hi':
-      sendText(sender, `Hey there, Howz you doing`)
-      break;
-    case 'how are you':
-      sendText(sender, 'I am good, what about you')
-      break;
-    default:
-      sendText(sender, 'Chatbot echo : ' + text.substring(0, 100))
-      break;
+  console.log('sender details are ---', sender);
+  if (
+    message.includes('hey') ||
+    message.includes('hello') ||
+    message.includes('hi') ||
+    message.includes('hlo')
+  ) {
+    greeting(sender);
+  } else if (message.includes('how are you')) {
+    sendText(sender, 'I am good, what about you');
+  } else {
+    sendText(sender, 'Chatbot echo : ' + text.substring(0, 100));
   }
-  // if(message.includes('hey') || message.includes('hello') || message.includes('hi')){
-  //   sendText(sender, `Hey there, Howz you doing`)
-  // } else if(message.includes('how are you')){
-  //   sendText(sender, 'I am good, what about you')
-  // } else {
-  //   sendText(sender, 'Chatbot echo : ' + text.substring(0, 100))
-  // }
-}
+};
