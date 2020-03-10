7d4fd3a3435fe41a7438b4cb42f0a1de6273d994
Ashish <cu.15bcs2313@gmail.com>
2020-03-10 18:29:25 +0530

    switch case implementation

--- processes/processMessage.js
+++ processes/processMessage.js
@@ -3,11 +3,22 @@ const sendText = require('./sendText');
 module.exports = function processMessage(sender, text){
   let message = text.toLowerCase();
   console.log('sender details are ---', sender)
-  if(message.includes('hey') || message.includes('hello') || message.includes('hi')){
-    sendText(sender.id, `Hey ${sender.name}, Howz you doing`)
-  } else if(message.includes('how are you')){
-    sendText(sender.id, 'I am good, what about you')
-  } else {
-    sendText(sender.id, 'Chatbot echo : ' + text.substring(0, 100))
+  switch(message){
+    case message.includes('hey') || message.includes('hello') || message.includes('hi'):
+      sendText(sender, `Hey there, Howz you doing`)
+      break;
+    case message.includes('how are you'):
+      sendText(sender, 'I am good, what about you')
+      break;
+    default:
+      sendText(sender, 'Chatbot echo : ' + text.substring(0, 100))
+      break;
   }
+  // if(message.includes('hey') || message.includes('hello') || message.includes('hi')){
+  //   sendText(sender, `Hey there, Howz you doing`)
+  // } else if(message.includes('how are you')){
+  //   sendText(sender, 'I am good, what about you')
+  // } else {
+  //   sendText(sender, 'Chatbot echo : ' + text.substring(0, 100))
+  // }
 }
