b7b5c598992b5b3ffffc2678a4ba54a064662289
Ashish <cu.15bcs2313@gmail.com>
2020-03-10 18:12:43 +0530

    includes fix and console

--- processes/processMessage.js
+++ processes/processMessage.js
@@ -2,9 +2,10 @@ const sendText = require('./sendText');
 
 module.exports = function processMessage(sender, text){
   let message = text.toLowerCase();
+  console.log('sender details are ---', sender)
   if(message.includes('hey') || message.includes('hello') || message.includes('hi')){
     sendText(sender.id, `Hey ${sender.name}, Howz you doing`)
-  } else if(message.include('how are you')){
+  } else if(message.includes('how are you')){
     sendText(sender.id, 'I am good, what about you')
   } else {
     sendText(sender.id, 'Chatbot echo : ' + text.substring(0, 100))
