8b9229552146d63f8e28f0ecfb0c9ae8bd7b6b3e
ashish <ashish@1touch.market>
2020-03-11 15:21:16 +0530

    helpIntent multiline fix

--- intents/helpIntent.js
+++ intents/helpIntent.js
@@ -1,14 +1,12 @@
 const sendText = require('../processes/sendText');
 
-module.exports = async function helpIntent(sender){
-    message1 = 'Sorry, I didn\'t get that';
-    message2 = 'If you need assistance press';
-    message3 = '1. Excercise Recommendations';
-    message4 = '2. Excercise Info';
-    message5 = '3. Excersice Images';
-    await sendText(sender, message1);
-    await sendText(sender, message2);
-    await sendText(sender, message3);
-    await sendText(sender, message4);
-    await sendText(sender, message5);
+module.exports = function helpIntent(sender){
+    message = `
+        Sorry, I didn't get that
+        If you need assistance press
+        1. Excercise Recommendations
+        2. Excercise Info
+        3. Excersice Images
+    `;
+    sendText(sender, message);
 }
