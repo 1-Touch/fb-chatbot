3a5d2278222d5e87fdcdbb40c02d3ea41a996a52
ashish <ashish@1touch.market>
2020-03-11 16:21:24 +0530

    excercise reccomendation using api

--- intents/excerciseReccomendIntent.js
+++ intents/excerciseReccomendIntent.js
@@ -1,17 +1,16 @@
-const request = require('request');
+const sendText = require('../processes/sendText');
 
 module.exports = function excerciseRecommendIntent(sender){
-    let excercises = request({
-        url: 'https://wger.de/api/v2/exercise',
-        method: 'GET',
-        function(error, response) {
-          if (error) {
-            console.log('sending error');
-          } else if (response.body.error) {
-            console.log('response body error');
-          }
-        }
-      });
-
-      console.log('the excercise variable', excercises)
+    fetch('https://wger.de/api/v2/exercise')
+    .then(data => data.json()
+    .then(res => {
+        const randomExcercise = Math.floor(Math.random() * res.results.length);
+        sendText(sender, `Name : ${res.results[randomExcercise].name}`);
+        sendText(sender, `Excercise : ${res.results[randomExcercise].description}`);
+    })
+    )
+    .catch(err => {
+        console.log(err)
+        sendText(sender, 'There was issue while reccomending excercise');
+    })
 }
