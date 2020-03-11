552f9e91aad1acf41e9747ab5c46deca007948a0
ashish <ashish@1touch.market>
2020-03-11 16:54:18 +0530

    data.results ndefined fix

--- intents/excerciseReccomendIntent.js
+++ intents/excerciseReccomendIntent.js
@@ -3,11 +3,11 @@ const axios = require('axios');
 
 module.exports = function excerciseRecommendIntent(sender){
     axios.get('https://wger.de/api/v2/exercise')
-    .then(data => {
-        console.log('the data is',data);
-        const randomExcercise = Math.floor(Math.random() * data.results.length);
-        sendText(sender, `Name : ${data.results[randomExcercise].name}`);
-        sendText(sender, `Excercise : ${data.results[randomExcercise].description}`);
+    .then(excercise => {
+        console.log('the excercise is',excercise.data);
+        const randomExcercise = Math.floor(Math.random() * excercise.data.results.length);
+        sendText(sender, `Name : ${excercise.data.results[randomExcercise].name}`);
+        sendText(sender, `Excercise : ${excercise.data.results[randomExcercise].description}`);
     //     .then(res => {
     //     const randomExcercise = Math.floor(Math.random() * res.results.length);
     //     sendText(sender, `Name : ${res.results[randomExcercise].name}`);
