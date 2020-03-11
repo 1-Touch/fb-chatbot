e841ccc6e60cc3bb62b301769d49b6ebc168bcba
ashish <ashish@1touch.market>
2020-03-11 16:34:09 +0530

    res.json bug

--- intents/excerciseReccomendIntent.js
+++ intents/excerciseReccomendIntent.js
@@ -3,8 +3,7 @@ const axios = require('axios');
 
 module.exports = function excerciseRecommendIntent(sender){
     axios.get('https://wger.de/api/v2/exercise')
-    .then(data => data.json()
-    .then(res => {
+    .then(data => data.json().then(res => {
         const randomExcercise = Math.floor(Math.random() * res.results.length);
         sendText(sender, `Name : ${res.results[randomExcercise].name}`);
         sendText(sender, `Excercise : ${res.results[randomExcercise].description}`);
