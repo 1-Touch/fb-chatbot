28fc8117e3b70206c7be9eaef6fb7fbc85e8a165
ashish <ashish@1touch.market>
2020-03-11 15:05:51 +0530

    helpIntent fix

--- intents/helpIntent.js
+++ intents/helpIntent.js
@@ -1,14 +1,14 @@
 const sendText = require('../processes/sendText');
 
-module.exports = function helpIntent(sender){
+module.exports = async function helpIntent(sender){
     message1 = 'Sorry, I didn\'t get that';
     message2 = 'If you need assistance press';
     message3 = '1. Excercise Recommendations';
     message4 = '2. Excercise Info';
     message5 = '3. Excersice Images';
-    sendText(sender, message1);
-    sendText(sender, message2);
-    sendText(sender, message3);
-    sendText(sender, message4);
-    sendText(sender, message5);
+    await sendText(sender, message1);
+    await sendText(sender, message2);
+    await sendText(sender, message3);
+    await sendText(sender, message4);
+    await sendText(sender, message5);
 }
