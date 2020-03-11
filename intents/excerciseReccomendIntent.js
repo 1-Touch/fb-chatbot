6f2dfdd1137103fe6631777f60bd116b28efbf8c
ashish <ashish@1touch.market>
2020-03-11 16:40:59 +0530

    excercise reccomendation debugging

--- intents/excerciseReccomendIntent.js
+++ intents/excerciseReccomendIntent.js
@@ -3,12 +3,14 @@ const axios = require('axios');
 
 module.exports = function excerciseRecommendIntent(sender){
     axios.get('https://wger.de/api/v2/exercise')
-    .then(data => data.json().then(res => {
+    .then(data => {
+        console.log('the data is',data);
+        data.json().then(res => {
         const randomExcercise = Math.floor(Math.random() * res.results.length);
         sendText(sender, `Name : ${res.results[randomExcercise].name}`);
         sendText(sender, `Excercise : ${res.results[randomExcercise].description}`);
     })
-    )
+    })
     .catch(err => {
         console.log(err)
         sendText(sender, 'There was issue while reccomending excercise');
